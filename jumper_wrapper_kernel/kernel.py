72fa5a283357f0c1a00ac65089e6eb5935372c38
OutlyingWest <alexeybuv7@gmail.com>
2026-02-11 20:37:37 +0100

    wrapper prefix for wrapped kernels

--- jumper_wrapper_kernel/kernel.py
+++ jumper_wrapper_kernel/kernel.py
@@ -328,7 +328,7 @@ class JumperWrapperKernel(IPythonKernel):
                     '-f', '{connection_file}',
                     '--JumperWrapperKernel.auto_wrap_kernel=' + wrapped_kernel_name,
                 ],
-                'display_name': new_kernel_name,
+                'display_name': f'Jumper Wrapper ({new_kernel_name})',
                 'language': wrapped_spec.get('language', 'python'),
                 'metadata': {
                     'debugger': False,
